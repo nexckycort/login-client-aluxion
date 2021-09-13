2d822f6fde551da292f46f2949da0c3f87f9d031
Nestor Cortina <junior110120@gmail.com>
2021-09-12 23:36:27 -0500

    feat: add logout

--- src/infrastructure/views/pages/Home/Home.tsx
+++ src/infrastructure/views/pages/Home/Home.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import { useAuth } from 'infrastructure/views/hooks'
+import { Button } from 'infrastructure/views/components/ui'
+
 const Home: React.FC = () => {
+  const { auth, logout } = useAuth()
+  const { user } = auth
+
   return (
     <div className="center">
-      <h1>HomeView</h1>
+      <h1>Hello {user.name}</h1>
+      <Button type="button" onClick={async () => await logout()}>
+        Logout
+      </Button>
     </div>
   )
 }
